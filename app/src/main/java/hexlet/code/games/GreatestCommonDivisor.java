12b6830cd0f7790bdbdf92bc039f1743675029ed
SerzhSterl <serzhsterl@gmail.com>
2023-08-06 01:24:16 +0300

    Small changes GCD class

--- app/src/main/java/hexlet/code/games/GreatestCommonDivisor.java
+++ app/src/main/java/hexlet/code/games/GreatestCommonDivisor.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class GreatestCommonDivisor {
     public static void startGreatestCommonDivisor() {
 
-        int randBound = 20;
+        int randBound = 100;
         var gameSteps = 3;
         int rightAnswer;
         int userAnswer;
